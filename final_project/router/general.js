4790011b4d6fe470e79657f67542058d1bbd5c85
Yulian Bedoya <ybedoyab@unal.edu.co>
2024-09-18 04:06:26 -0500

    getbooksbyauthor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,10 +29,17 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+
+  if (filteredBooks.length > 0) {
+      res.status(200).json(filteredBooks);
+  } else {
+      res.status(404).json({ message: "No books found by this author" });
+  }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
