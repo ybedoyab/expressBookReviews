07283d160f02aebeb7d471153f7bd32d090a42c3
Yulian Bedoya <ybedoyab@unal.edu.co>
2024-09-18 04:03:55 -0500

    gedetailsISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,16 @@ public_users.get('/', function (req, res) {
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn]; // Find book by ISBN
+  if (book) {
+      res.status(200).json(book);
+  } else {
+      res.status(404).json({ message: "Book not found" });
+  }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
