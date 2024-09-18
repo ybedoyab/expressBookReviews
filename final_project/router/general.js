e7856ede55a5ebaca29782338bf3817a051b62cc
Yulian Bedoya <ybedoyab@unal.edu.co>
2024-09-18 03:42:34 -0500

    tasks 1-9

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,33 +11,122 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  res.status(200).json(books); // Return all books
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn]; // Find book by ISBN
+  if (book) {
+      res.status(200).json(book);
+  } else {
+      res.status(404).json({ message: "Book not found" });
+  }
+});
+
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  const filteredBooks = Object.values(books).filter(book => book.author === author);
+
+  if (filteredBooks.length > 0) {
+      res.status(200).json(filteredBooks);
+  } else {
+      res.status(404).json({ message: "No books found by this author" });
+  }
 });
+
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  const filteredBooks = Object.values(books).filter(book => book.title === title);
+
+  if (filteredBooks.length > 0) {
+      res.status(200).json(filteredBooks);
+  } else {
+      res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+// Get book review based on ISBN
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  
+  if (book && book.reviews) {
+      res.status(200).json(book.reviews);
+  } else {
+      res.status(404).json({ message: "No reviews found for this book" });
+  }
 });
 
-module.exports.general = public_users;
+public_users.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  
+  if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  const userExists = users.some(user => user.username === username);
+  
+  if (userExists) {
+      return res.status(400).json({ message: "Username already exists" });
+  }
+
+  users.push({ username, password });
+  res.status(200).json({ message: "User registered successfully" });
+});
+
+public_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (user) {
+      // You can also implement session or JWT logic here
+      return res.status(200).json({ message: "Login successful" });
+  } else {
+      return res.status(401).json({ message: "Invalid credentials" });
+  }
+});
+
+// Add or modify a review
+public_users.put('/review/:isbn', (req, res) => {
+  const { review } = req.body;
+  const isbn = req.params.isbn;
+
+  if (!review) {
+      return res.status(400).json({ message: "Review content is required" });
+  }
+
+  const book = books[isbn];
+  
+  if (book) {
+      book.reviews.push(review);
+      res.status(200).json({ message: "Review added/modified successfully" });
+  } else {
+      res.status(404).json({ message: "Book not found" });
+  }
+});
+
+// Delete a review
+public_users.delete('/review/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book && book.reviews) {
+      book.reviews = [];  // Assuming users can delete all their reviews
+      res.status(200).json({ message: "Review deleted successfully" });
+  } else {
+      res.status(404).json({ message: "Review or book not found" });
+  }
+});
